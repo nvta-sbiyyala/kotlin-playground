3f1e6c432a1e9a419bbb62c2d3800abb9bcc7ebd
Shishir Biyyala <Shishir.Biyyala@invitae.com>
2020-02-10 16:45:12 -0500

    Understanding accessors

---
+++ src/playground/Rectangle.kt
@@ -0,0 +1,13 @@
+package playground
+
+import java.util.*
+
+class Rectangle(private val height: Int, private val width: Int) {
+    val isSquare: Boolean
+        get() = height == width
+}
+
+fun createRandomRectangle(): Rectangle {
+    val random = Random()
+    return Rectangle(random.nextInt(), random.nextInt())
+}
